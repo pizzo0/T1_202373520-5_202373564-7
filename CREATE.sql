426e81363dfbbda22fed154031378cdd082616ee
pizzo0 <caceresalejandro934@gmail.com>
2025-03-28 00:07:43 -0300

    xd

--- CREATE.sql
+++ CREATE.sql
@@ -1,23 +1,30 @@
 CREATE DATABASE gescon;
-\c gescon;                                  --postgreSQL
+\c gescon;                                      --postgreSQL
+
+CREATE DOMAIN "Rut" VARCHAR(10) NOT NULL;       --formato: 11111111-1
+CREATE DOMAIN "Nombre" VARCHAR(255) NOT NULL;
+CREATE DOMAIN "Email" VARCHAR(255) NOT NULL;
+
+CREATE DOMAIN "Fecha" DATE NOT NULL;
+
 
 CREATE TABLE Autores (
-    rut VARCHAR(10) PRIMARY KEY,            --formato: 11111111-1
-    nombre VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL
+    rut "Rut" PRIMARY KEY,
+    nombre "Nombre",
+    email "Email"
 );
 
 CREATE TABLE Revisores (
-    rut VARCHAR(10) PRIMARY KEY,            --formato: 11111111-1
-    nombre VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    rut "Rut" PRIMARY KEY,
+    nombre "Nombre",
+    email "Email",
     especialidad VARCHAR(100) NOT NULL
-)
+);
 
 CREATE TABLE Articulos (
     id_articulo SERIAL PRIMARY KEY,
     titulo VARCHAR(255) NOT NULL,
-    fecha_envio DATE NOT NULL,
+    fecha_envio "Fecha",
     resumen VARCHAR(150) NOT NULL,
     topicos VARCHAR(255) NOT NULL
-)
+);
